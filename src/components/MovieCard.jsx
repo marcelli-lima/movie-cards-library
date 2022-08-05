bf29e26a711033efdaeb13857fb73e953230a4d0
marcelli-lima <marcelli_lima@hotmail.com>
2022-08-05 11:05:36 -0300

    adiciona componentes MovieCard e MovieList

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,14 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath, rating} = movie;
+    return (
+      <>
+      </>
+    )
+  }
+}
+
+export default MovieCard;
