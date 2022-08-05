921fcd2f45bc1236a2c167fda129fa31b850a378
marcelli-lima <marcelli_lima@hotmail.com>
2022-08-05 11:15:14 -0300

    renderizando titulo, subtitulo e sinopse dos filmes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,7 +6,10 @@ class MovieCard extends React.Component {
     const { title, subtitle, storyline, imagePath, rating} = movie;
     return (
       <div>
-        <img src={imagePath} alt={ title } />
+        <img src={ imagePath } alt={ title } />
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
       </div>
     )
   }
