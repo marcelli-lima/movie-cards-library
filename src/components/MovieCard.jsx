07be95d59b54a0627cf7ee5a722d8cf044d60d48
marcelli-lima <marcelli_lima@hotmail.com>
2022-08-05 11:11:13 -0300

    adiciona imagem dos filmes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,8 +5,9 @@ class MovieCard extends React.Component {
     const { movie } = this.props;
     const { title, subtitle, storyline, imagePath, rating} = movie;
     return (
-      <>
-      </>
+      <div>
+        <img src={imagePath} alt={ title } />
+      </div>
     )
   }
 }
